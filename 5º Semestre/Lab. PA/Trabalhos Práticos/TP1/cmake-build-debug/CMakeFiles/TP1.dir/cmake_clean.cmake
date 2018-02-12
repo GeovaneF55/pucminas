4d6735dcefb8dc3b658e109df959e2a08546f52c
GeovaneF55 <geovanefss@gmail.com>
2018-02-12 10:02:37 -0200

    TP1 LPA 12_02_18 força bruta

---
+++ 5º Semestre/Lab. PA/Trabalhos Práticos/TP1/cmake-build-debug/CMakeFiles/TP1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1.dir/main.c.o"
+  "TP1.pdb"
+  "TP1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
