880b5dfc16f9817592af5d6dff821b4e6536c66e
GeovaneF55 <geovanefss@gmail.com>
2018-05-17 00:12:05 -0300

    ADA 17_05_2018 qrCodeGenerator pyhton

--- Extraclasse/ada/Gerador de QRCode/qrCode.py
+++ Extraclasse/ada/Gerador de QRCode/qrCode.py
@@ -1,9 +1,35 @@
 import pyqrcode
 import qrtools
 
-qr = pyqrcode.create("HORN O.K. PLEASE.")
-qr.png("horn.png", scale=6)
-qr = qrtools.QR()
-qr.decode("horn.png")
-True
-print(qr.data)
+def qrCode( code ):
+    qr = pyqrcode.create( code )
+    qr.png( "matriculas/" + code + ".eps", scale=6 )
+    qr = qrtools.QR()
+    return
+
+def qrDecode( decode ):
+    qr = pyqrcode.create( decode )
+    qr = qrtools.QR()
+    qr.decode( "matriculas/" + decode + ".eps" )
+    True
+    print(qr.data)
+    return
+
+def process( row , code ):
+    vector = row.replace(' ', '').rstrip().split('/')
+    matricula = vector[0]
+    nome = vector[1]
+    if code:
+        qrCode( matricula )
+    else:
+        qrDecode( matricula )
+    return
+
+input_path = 'matriculas.in'
+
+with open( input_path, 'rb' ) as f:
+    while True:
+        row = f.readline()
+        if not row: break
+        process( row , True )
+        process( row , False )
