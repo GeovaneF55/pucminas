e111e4f50fca1b7f8e99abda3f4f21dcfa2c8633
GeovaneF55 <geovanefss@gmail.com>
2018-11-12 19:58:22 -0200

    Redes I 12_11_2018 trabalho prático - batalha naval: servidor e cliente

--- 6º Semestre/Redes I/Trabalho Prático/batalha naval/client_nb.py
+++ 6º Semestre/Redes I/Trabalho Prático/batalha naval/client_nb.py
@@ -1,13 +1,23 @@
 ############## client_tuto.py################# #!/usr/bin/python 
 # # Coded by: Alisson Menezes [kernelcrash] # # 
 import socket
-ip = input('digite o ip de conexao: ')
-port = 7000
-addr = ((ip,port))
+
+# Recebe o ip e a porta do input para criar o endereço
+my_socket = input()
+ip, port = my_socket.split(" ")
+addr = (ip, int(port))
+
+# Cria o socket:
+# declara a família do protocolo através AF_INET
+# declara o protocolo como tcp/ip através do SOCKET_STREAM
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(addr)
-mensagem = input('digite uma mensagem para enviar ao servidor: ')
+
+# Cria a mensagem e envia para o servidor
+mensagem = 'Busque o conhecimento'
 client_socket.send(mensagem.encode())
 print('mensagem enviada')
+
+# Fecha a conexão
 client_socket.close()
 ############=----EOF----###########
--- 6º Semestre/Redes I/Trabalho Prático/batalha naval/server_nb.py
+++ 6º Semestre/Redes I/Trabalho Prático/batalha naval/server_nb.py
@@ -2,18 +2,34 @@
 # # Coded by: Alisson Menezes [kernelcrash]# #
 # # servidor que recebe mensagens de aplicação client parecido com o netsend # #
 import socket
+
+# Recebe a porta do input para criar o endereço
+my_socket = input()
 host = ''
-port = 7000
-addr = (host, port)
+port = my_socket
+addr = (host, int(port))
+
+# Cria o socket:
+# declara a família do protocolo através AF_INET
+# declara o protocolo como tcp/ip através do SOCKET_STREAM
+# Zera o tempo de espera do socket ao interromper o programa através do 'setsockopt'
 serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serv_socket.bind(addr)
+
+# Define um limite de 10 conexões
 serv_socket.listen(10)
 print('aguardando conexao')
+
+# Deixa o servidor escutando possíveis conexões
 con, cliente = serv_socket.accept()
 print('conectado')
+
+# Aguarda um dado enviado pela rede de até 1024 Bytes
 print('aguardando mensagem')
 recebe = con.recv(1024)
 print('mensagem recebida: ' + recebe.decode('utf8').strip())
+
+# Fecha a conexão
 serv_socket.close()
 #############=====EOF======###################
