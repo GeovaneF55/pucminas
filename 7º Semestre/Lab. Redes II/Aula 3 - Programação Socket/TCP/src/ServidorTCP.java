4fdaaf9e58e95cf222dcada9eb7b70e069fbff8f
GeovaneF55 <geovanefss@gmail.com>
2019-02-23 12:46:18 -0300

    materias 23_02_19 materiais

---
+++ 7º Semestre/Lab. Redes II/Aula 3 - Programação Socket/TCP/src/ServidorTCP.java
@@ -0,0 +1,45 @@
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.*;
+
+public class ServidorTCP
+{
+	//MÉTODO PRINCIPAL DA CLASSE
+	public static void main (String args[])
+	{
+		try
+		{
+			int PortaServidor = 7000;
+			
+			//INICIALIZA UM SERVIÇO DE ESCUTA POR CONEXÕES NA PORTA ESPECIFICADA
+			System.out.println(" -S- Aguardando conexao (P:"+PortaServidor+")...");
+			ServerSocket socktServ = new ServerSocket(PortaServidor);
+			
+			//ESPERA (BLOQUEADO) POR CONEXÕES			
+			Socket conSer = socktServ.accept(); //RECEBE CONEXÃO E CRIA UM NOVO CANAL (p) NO SENTIDO CONTRÁRIO (SERVIDOR -> CLIENTE)
+			System.out.println(" -S- Conectado ao cliente ->" + conSer.toString());
+		
+			//CRIA UM PACOTE DE ENTRADA PARA RECEBER MENSAGENS, ASSOCIADO À CONEXÃO (p)
+			ObjectInputStream sServIn = new ObjectInputStream(conSer.getInputStream());
+			System.out.println(" -S- Recebendo mensagem...");
+			Object msgIn = sServIn.readObject(); //ESPERA (BLOQUEADO) POR UM PACOTE
+			System.out.println(" -S- Recebido: " + msgIn.toString());
+			
+			//CRIA UM PACOTE DE SAÍDA PARA ENVIAR MENSAGENS, ASSOCIANDO-O À CONEXÃO (p)
+			ObjectOutputStream sSerOut = new ObjectOutputStream(conSer.getOutputStream());
+			sSerOut.writeObject("RETORNO " + msgIn.toString() + " - TCP"); //ESCREVE NO PACOTE
+			System.out.println(" -S- Enviando mensagem resposta...");
+			sSerOut.flush(); //ENVIA O PACOTE
+		
+			//FINALIZA A CONEXÃO
+			socktServ.close();
+			conSer.close();
+			System.out.println(" -S- Conexao finalizada...");
+
+		}
+		catch(Exception e) //SE OCORRER ALGUMA EXCESSÃO, ENTÃO DEVE SER TRATADA (AMIGAVELMENTE)
+		{
+			System.out.println(" -S- O seguinte problema ocorreu : \n" + e.toString());
+		}
+	}
+}
